b7e80d4efde02671e757bfc9835b0221a445c094
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-09-08 04:58:42 +0600

    refactor: Update splash screen logo import path

--- lib/features/rocket/splash/ui/splash.dart
+++ lib/features/rocket/splash/ui/splash.dart
@@ -1,7 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:flutter/material.dart';
-import 'package:rocket/features/rocket/home/ui/home.dart';
+import 'package:rocket/features/bottom_nav/ui/bottom_nav.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
@@ -13,7 +13,7 @@ class Splash extends StatelessWidget {
         duration: 3000,
         splash: "assets/logos/logoDark.png", 
         splashIconSize: MediaQuery.of(context).size.width / 2,
-        nextScreen: const Home(),
+        nextScreen: const BottomNav(),
         splashTransition: SplashTransition.fadeTransition,
         pageTransitionType: PageTransitionType.fade,
         );
