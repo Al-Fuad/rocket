ac61194f160c1ba201d4a37213ec8ca932981a8f
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-09-08 07:29:43 +0600

    feat: Add BottomNavBloc and update dependencies

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rocket/features/bottom_nav/bloc/bottom_nav_bloc.dart';
 import 'package:rocket/features/rocket/splash/ui/splash.dart';
 
 class MainApp extends StatelessWidget {
@@ -7,17 +9,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          textTheme: GoogleFonts.pressStart2pTextTheme(
-            Theme.of(context).textTheme.apply(
-              bodyColor: Colors.white,
-              displayColor: Colors.white,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => BottomNavBloc(),
+        ),
+      ],
+      child: MaterialApp(
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            textTheme: GoogleFonts.pressStart2pTextTheme(
+              Theme.of(context).textTheme.apply(
+                    bodyColor: Colors.white,
+                    displayColor: Colors.white,
+                  ),
             ),
           ),
-        ),
-        debugShowCheckedModeBanner: false,
-        home: const Splash());
+          debugShowCheckedModeBanner: false,
+          home: const Splash()),
+    );
   }
 }
