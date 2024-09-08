a2f59524dbb6ac1f965a3a75ffe01b3ff2c7c613
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-09-08 07:30:01 +0600

    feat: Add opacity property to Background widget

--- lib/common/widgets/backgrounds/background.dart
+++ lib/common/widgets/backgrounds/background.dart
@@ -4,10 +4,12 @@ class Background extends StatelessWidget {
   const Background(
       {super.key,
       required this.child,
-      this.background = 'assets/backgrounds/background.gif'});
+      this.background = 'assets/backgrounds/background.gif',
+      this.opacity = 1});
 
   final Widget child;
   final String background;
+  final double opacity;
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +18,9 @@ class Background extends StatelessWidget {
         width: double.infinity,
         height: double.infinity,
         child: Image.asset(
+          opacity: AlwaysStoppedAnimation(opacity),
           background,
-          fit: BoxFit.fitHeight
+          fit: BoxFit.cover
           ),
       ),
       child,
